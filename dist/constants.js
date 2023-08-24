446c737757f7792d1a568ccee36ead90395875b7
Diraj Thajali <thajalidiraj@gmail.com>
2023-08-24 00:32:53 -0700

    Remove 'dist' folder from Git tracking

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ActionSignatureTimestampHeader = exports.ActionEd25519SignatureHeader = exports.ActionEcdsaSignatureHeader = void 0;
-exports.ActionEcdsaSignatureHeader = "X-Signature-Ecdsa";
-exports.ActionEd25519SignatureHeader = "X-Signature-Ed25519";
-exports.ActionSignatureTimestampHeader = "X-Signature-Timestamp";
